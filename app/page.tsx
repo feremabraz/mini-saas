261ea78782e42291778650f447e58c11969ac9fb
Fernando Braz <hello@fernandobraz.online>
2024-06-19 08:28:19 +0100

    chore: usual defaults

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+import { Button } from '@/components/ui/button';
+
+export default function LandingPage() {
   return (
     <main>
-      <div>Hello world!</div>
+      <section className="container h-screen overflow-hidden bg-background text-foreground pt-24">
+        <div className="max-w-3xl mx-auto">
+          <h1 className="text-6xl text-center font-semibold mb-6">
+            Get testimonials from your customers with ease
+          </h1>
+          <p className="mb-5 text-center opacity-80">
+            Collecting testimonials is hard, we get it! So we built Testimonial.
+            In minutes, you can collect text and video testimonials from your
+            customers with no need for a developer or website hosting.
+          </p>
+          <div className="flex gap-2 justify-center items-center pt-6">
+            <Button size="lg">Get Started</Button>
+            <Button size="lg" variant="ghost">
+              Contact Us
+            </Button>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
