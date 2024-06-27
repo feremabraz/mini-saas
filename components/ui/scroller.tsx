7daa7545314b82ca61c35a00ae6be17d337dc5d7
Fernando Braz <hello@fernandobraz.online>
2024-06-27 15:30:25 +0100

    feat(ui): review option for the scroller

--- components/ui/scroller.tsx
+++ components/ui/scroller.tsx
@@ -7,73 +7,100 @@ import {
   TargetAndTransition,
   Transition,
 } from 'framer-motion';
-import { Bone, Bug, Cat, PawPrint } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
-const logos = [
-  { icon: Bone, name: 'Bone' },
-  { icon: Bug, name: 'Bug' },
-  { icon: Cat, name: 'Cat' },
-  { icon: PawPrint, name: 'Paw' },
-];
+export interface Logo {
+  icon: React.ElementType;
+  name: string;
+}
 
 export interface ScrollerProps extends React.HTMLAttributes<HTMLDivElement> {
   animate?: AnimationProps['animate'];
   transition?: Transition;
+  children?: React.ReactNode;
+  logos?: Logo[];
+  useLogos?: boolean;
 }
 
 const Scroller = React.forwardRef<HTMLDivElement, ScrollerProps>(
-  ({ className, animate, transition, ...props }, ref) => {
+  (
+    {
+      className,
+      animate,
+      transition,
+      children,
+      logos = [],
+      useLogos = false,
+      ...props
+    },
+    ref
+  ) => {
     const defaultAnimate: TargetAndTransition = {
-      x: [0, -100 * logos.length * 2],
+      x: useLogos ? [0, -100 * logos.length * 2] : [0, '-100%'],
     };
 
     const defaultTransition: Transition = {
       x: {
         repeat: Infinity,
         repeatType: 'loop',
-        duration: 40,
+        duration: useLogos ? 40 : 20,
         ease: 'linear',
       },
     };
 
     const getAnimate = (): AnimationProps['animate'] => {
-      if (typeof animate === 'function') {
-        return animate;
+      if (animate) return animate;
+      return defaultAnimate;
+    };
+
+    const renderContent = () => {
+      if (useLogos && logos.length > 0) {
+        return (
+          <>
+            {[...Array(4)].map((_, i) => (
+              <div
+                key={i}
+                className="flex shrink-0 justify-around gap-8 flex-row"
+              >
+                {logos.map((logo, index) => (
+                  <div key={index} className="flex items-center w-28 gap-3">
+                    <logo.icon size={24} />
+                    <p className="text-lg font-bold">{logo.name}</p>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </>
+        );
+      } else {
+        return (
+          <>
+            {children}
+            {children}
+          </>
+        );
       }
-      if (Array.isArray(animate) || typeof animate === 'string') {
-        return animate;
-      }
-      return { ...defaultAnimate, ...(animate as TargetAndTransition) };
     };
 
     return (
       <div
         ref={ref}
-        className={cn('overflow-hidden py-2 max-w-full relative', className)}
+        className={cn('overflow-hidden py-2 w-full relative', className)}
         {...props}
       >
         <motion.div
-          className="flex flex-row"
+          className={cn('flex', useLogos ? 'flex-row' : 'flex-nowrap')}
           animate={getAnimate()}
           transition={transition || defaultTransition}
         >
-          {[...Array(4)].map((_, i) => (
-            <div
-              key={i}
-              className="flex shrink-0 justify-around gap-8 flex-row"
-            >
-              {logos.map((logo, index) => (
-                <div key={index} className="flex items-center w-28 gap-3">
-                  <logo.icon size={24} />
-                  <p className="text-lg font-bold">{logo.name}</p>
-                </div>
-              ))}
-            </div>
-          ))}
+          {renderContent()}
         </motion.div>
-        <div className="absolute inset-y-0 left-0 w-1/6 bg-gradient-to-r from-white to-transparent" />
-        <div className="absolute inset-y-0 right-0 w-1/6 bg-gradient-to-l from-white to-transparent" />
+        {!useLogos && (
+          <>
+            <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background" />
+            <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background" />
+          </>
+        )}
       </div>
     );
   }
--- stories/sections/scroller.stories.tsx
+++ stories/sections/scroller.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Scroller } from '@/components/ui/scroller';
+import { Scroller, Logo } from '@/components/ui/scroller';
+import { cn } from '@/lib/utils';
+import { Bone, Bug, Cat, PawPrint } from 'lucide-react';
+import BoringAvatar from 'boring-avatars';
 
 const meta: Meta<typeof Scroller> = {
   title: 'Components/Scroller',
@@ -10,23 +13,152 @@ const meta: Meta<typeof Scroller> = {
 export default meta;
 type Story = StoryObj<typeof Scroller>;
 
-export const Default: Story = {
-  args: {},
+const logos: Logo[] = [
+  { icon: Bone, name: 'Bone' },
+  { icon: Bug, name: 'Bug' },
+  { icon: Cat, name: 'Cat' },
+  { icon: PawPrint, name: 'Paw' },
+];
+
+export const LogoScroller: Story = {
+  args: {
+    useLogos: true,
+    logos: logos,
+  },
 };
 
-export const CustomAnimation: Story = {
+const reviews = [
+  {
+    name: 'Jack',
+    username: '@jack',
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: 'https://avatar.vercel.sh/jack',
+  },
+  {
+    name: 'Jill',
+    username: '@jill',
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: 'https://avatar.vercel.sh/jill',
+  },
+  {
+    name: 'John',
+    username: '@john',
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: 'https://avatar.vercel.sh/john',
+  },
+  {
+    name: 'Jane',
+    username: '@jane',
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: 'https://avatar.vercel.sh/jane',
+  },
+  {
+    name: 'Jenny',
+    username: '@jenny',
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: 'https://avatar.vercel.sh/jenny',
+  },
+  {
+    name: 'James',
+    username: '@james',
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: 'https://avatar.vercel.sh/james',
+  },
+];
+
+const boringColors = ['#BBB5BD', '#BDBABD', '#C4BAB9'];
+
+const ReviewCard = ({
+  name,
+  username,
+  body,
+}: {
+  name: string;
+  username: string;
+  body: string;
+}) => {
+  return (
+    <figure
+      className={cn(
+        'relative cursor-pointer overflow-hidden rounded-xl border p-4',
+        'border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]',
+        'dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]'
+      )}
+      style={{ width: '280px' }}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <div className="flex items-center justify-center">
+          <BoringAvatar
+            size={24}
+            name={name}
+            variant="beam"
+            colors={boringColors}
+            square={false}
+          />
+        </div>
+        <div className="flex flex-col">
+          <figcaption className="text-sm font-medium dark:text-white">
+            {name}
+          </figcaption>
+          <p className="text-xs font-medium dark:text-white/40">{username}</p>
+        </div>
+      </div>
+      <blockquote className="mt-2 text-sm">{body}</blockquote>
+    </figure>
+  );
+};
+
+export const ReviewScroller: Story = {
   args: {
-    animate: { x: [0, 500], opacity: [1, 0.5, 1] },
-    transition: {
-      x: {
-        duration: 20,
-        repeat: Infinity,
-      },
-      opacity: {
-        duration: 2,
-        repeat: Infinity,
-        repeatType: 'reverse',
-      },
-    },
+    useLogos: false,
+  },
+  render: (args) => {
+    const scrollerContent = (
+      <>
+        <div className="flex gap-4">
+          {reviews.map((review) => (
+            <ReviewCard key={review.username} {...review} />
+          ))}
+        </div>
+        {/* Add an extra gap at the end */}
+        <div className="w-4 flex-shrink-0"></div>
+      </>
+    );
+
+    return (
+      <div className="relative w-full overflow-hidden py-10">
+        <Scroller
+          {...args}
+          className="mb-4"
+          animate={{ x: ['0%', '-50%'] }}
+          transition={{
+            x: {
+              duration: 20,
+              repeat: Infinity,
+              repeatType: 'loop',
+              ease: 'linear',
+            },
+          }}
+        >
+          {scrollerContent}
+          {scrollerContent}
+        </Scroller>
+        <Scroller
+          {...args}
+          animate={{ x: ['-50%', '0%'] }}
+          transition={{
+            x: {
+              duration: 15,
+              repeat: Infinity,
+              repeatType: 'loop',
+              ease: 'linear',
+            },
+          }}
+        >
+          {scrollerContent}
+          {scrollerContent}
+        </Scroller>
+      </div>
+    );
   },
 };
