1c58d5cdcf96adaa5453a74a578a5c85248cb06e
Fernando Braz <hello@fernandobraz.online>
2024-07-01 14:07:08 +0100

    fix: cleanup

--- prisma/zod/user.ts
+++ prisma/zod/user.ts
@@ -1,15 +1,15 @@
-import * as z from "zod"
-import { CompleteSession, relatedSessionSchema } from "./index"
+import * as z from 'zod';
+import { CompleteSession, relatedSessionSchema } from './index';
 
 export const userSchema = z.object({
   id: z.string(),
   email: z.string(),
   hashedPassword: z.string(),
   name: z.string().nullish(),
-})
+});
 
 export interface CompleteUser extends z.infer<typeof userSchema> {
-  sessions: CompleteSession[]
+  sessions: CompleteSession[];
 }
 
 /**
@@ -17,6 +17,8 @@ export interface CompleteUser extends z.infer<typeof userSchema> {
  *
  * NOTE: Lazy required in case of potential circular dependencies within schema
  */
-export const relatedUserSchema: z.ZodSchema<CompleteUser> = z.lazy(() => userSchema.extend({
-  sessions: relatedSessionSchema.array(),
-}))
+export const relatedUserSchema: z.ZodSchema<CompleteUser> = z.lazy(() =>
+  userSchema.extend({
+    sessions: relatedSessionSchema.array(),
+  })
+);
