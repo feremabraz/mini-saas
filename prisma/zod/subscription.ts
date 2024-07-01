68cb7f452cf28eb9b93c8fb14d78fd7d35af143d
Fernando Braz <hello@fernandobraz.online>
2024-07-01 14:06:31 +0100

    fix: cleanup

--- prisma/zod/subscription.ts
+++ prisma/zod/subscription.ts
@@ -1,4 +1,4 @@
-import * as z from 'zod';
+import * as z from "zod"
 
 export const subscriptionSchema = z.object({
   userId: z.string(),
@@ -6,4 +6,4 @@ export const subscriptionSchema = z.object({
   stripeSubscriptionId: z.string().nullish(),
   stripePriceId: z.string().nullish(),
   stripeCurrentPeriodEnd: z.date().nullish(),
-});
+})
